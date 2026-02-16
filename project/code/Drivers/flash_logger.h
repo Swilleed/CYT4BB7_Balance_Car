92dd415a6aa3fcd94fddb7da98042f9b7a4d7fbc
Will <willwupy@outlook.com>
2026-02-16 18:00:35 +0800

    微小改动，flash头文件加入宏保护

--- project/code/Drivers/flash_logger.h
+++ project/code/Drivers/flash_logger.h
@@ -1,6 +1,10 @@
 #ifndef _FLASH_LOGGER_H_
 #define _FLASH_LOGGER_H_
 
+#ifndef FLASH_PAGE_SIZE
+#define FLASH_PAGE_SIZE 256 // 假设每页大小为256字节，根据实际
+#endif
+
 void Logger_Init(void);
 void Logger_WriteBlock(uint32 start_page, void *buffer, uint32 byte_len);
 void Logger_ReadBlock(uint32 start_page, void *buffer, uint32 byte_len);
